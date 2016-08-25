280e7ca941ebba1c8a0661876cbb63a7c69def50
Arjun Ray <arayq2@gmail.com>
2016-08-24 22:18:26 -0400

    nested exception class
    boilerplate to declare and define an inner class based on standard
    exception hierarchy.

---
+++ DeclareException.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <stdexcept>
+#include <string>
+
+    // This should be a nested class and thus it needs no distinct
+    // name, as it will be namespaced by the containing class.
+    //
+#ifndef DECLARE_EXCEPTION
+#define DECLARE_EXCEPTION \
+    struct Exception : virtual public std::runtime_error\
+    { Exception(std::string const& msg) : std::runtime_error(msg) {} }
+#endif
