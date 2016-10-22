8305e9a5d00a185369ade6afab99e59f03d90c05
Arjun Ray <arayq2@gmail.com>
2016-10-22 14:39:13 -0400

    new class
    Cache.h - LRU cache implementation.

---
+++ Cache.h
@@ -0,0 +1,96 @@
+#pragma once
+
+#include <list>
+#include <map>
+#include <unordered_map>
+#include <functional>
+
+namespace Utility
+{
+    // Cache.h
+    // LRU eviction based cache.
+    // Note value type semantics (use smart pointers if needed)
+    //
+    template<typename Key, typename Value, template<typename...> class MapType>
+    class Cache
+    {
+    public:
+        using key_type   = Key;
+        using value_type = Value;
+        using Tracker    = std::list<key_type>; // stores keys in LRU order
+        using iterator   = typename Tracker::iterator;
+        using Pair       = std::pair<value_type, iterator>;
+        using Storage    = MapType<key_type, Pair>;
+        using Function   = std::function<value_type( key_type const& )>;
+        
+        Cache(std::size_t capacity, Function function)
+        : capacity_(capacity)
+        , function_(function)
+        , tracker_()
+        , storage_()
+        {}
+        
+        // mimics interface of function_
+        value_type operator() ( key_type const& key )
+        {
+            auto    _it(storage_.find( key ));
+            
+            if ( _it != storage_.end() )
+            {   // cache hit: move key to tail
+                tracker_.splice( tracker_.end(), tracker_, (*_it).second.second ); 
+                return (*_it).second.first;
+            }
+            // cache miss: get new value first (for exception safety)
+            value_type  _nv(function_( key ));
+            // cache new key/value (not completely safe!)
+            storage_[key] = std::make_pair(_nv, tracker_.insert( tracker_.end(), key ));
+            // evict LRU entry if needed
+            while ( storage_.size() > capacity_ )
+            {
+                storage_.erase( storage_.find( tracker_.front() ) );
+                tracker_.pop_front();
+            }
+            return _nv;
+        }
+        
+        void remove( key_type const& key )
+        {
+            auto    _it(storage_.find( key ));
+            
+            if ( _it != storage_.end() )
+            {   
+                tracker_.erase( (*_it).second.second ); 
+                storage_.erase( _it );
+            }            
+        }
+        
+        void clear()  // ouch
+        {
+            storage_.clear();
+            tracker_.clear();
+        }
+
+        // for iterating over keys
+        auto begin() -> typename Tracker::iterator const { return tracker_.begin(); }
+        auto end()   -> typename Tracker::iterator const { return tracker_.end();   }
+        
+        template<typename Action>
+        void apply( Action&& action ) const
+        {
+            for ( auto const& key : tracker_ ) { action( key ); }
+        }      
+
+    private:
+        std::size_t capacity_;
+        Function    function_;
+        Tracker     tracker_;
+        Storage     storage_;
+    };
+
+    template<typename Key, typename Value>
+    using TreeMapCache = Cache<Key, Value, std::map>;
+    
+    template<typename Key, typename Value>
+    using HashMapCache = Cache<Key, Value, std::unordered_map>;
+
+} // namespace Utility
---
+++ tests/test_cache.cpp
@@ -0,0 +1,90 @@
+
+#include "Cache.h"
+#include <string>
+#include <sstream>
+#include <iostream>
+
+    class ExpensiveFunction
+    {
+    public:
+        ExpensiveFunction(size_t& count)
+        : count_(count)
+        {}
+        
+        std::string operator() ( std::string const& input )
+        {
+            std::ostringstream  _oss;
+            _oss << input << "_val";
+            ++count_;
+            return _oss.str();
+        }
+        
+    private:
+        size_t&  count_;
+    };
+
+
+static size_t count = 0;
+    
+std::string expfn( std::string const& input )
+{
+    std::ostringstream  _oss;
+    _oss << input << "_val";
+    ++count;
+    return _oss.str();
+}
+
+void print_key( std::string const& key ) 
+{
+    std::cerr << "[" << key << "]";
+}
+  
+void test_tree()
+{
+    using TreeCache = Utility::TreeMapCache<std::string, std::string>;
+    TreeCache           _tcache(5, expfn);
+
+    std::cerr << "\nTesting TreeCache...\n";
+
+    std::cerr << "Caching one: "   << _tcache( "one" ) << "\n";
+    std::cerr << "Caching two: "   << _tcache( "two" ) << "\n";
+    std::cerr << "Caching three: " << _tcache( "three" ) << "\n";
+    std::cerr << "Caching four: "  << _tcache( "four" ) << "\n";
+    std::cerr << "Caching five: "  << _tcache( "five" ) << "\n";
+    
+    std::cerr << "Getting one: "   << _tcache( "one" ) << "\n";
+    std::cerr << "Caching six: "   << _tcache( "six" ) << "\n";
+    std::cerr << "Count: " << count << "\n";
+    _tcache.apply( print_key );
+    std::cerr << "\n";
+}
+ 
+void test_hash()
+{
+    using HashCache = Utility::HashMapCache<std::string, std::string>;
+    
+    size_t              _count{0};
+    ExpensiveFunction   _ef(_count);
+    HashCache           _hcache(5, _ef);
+
+    std::cerr << "\nTesting HashCache...\n";
+
+    std::cerr << "Caching one: "   << _hcache( "one" ) << "\n";
+    std::cerr << "Caching two: "   << _hcache( "two" ) << "\n";
+    std::cerr << "Caching three: " << _hcache( "three" ) << "\n";
+    std::cerr << "Caching four: "  << _hcache( "four" ) << "\n";
+    std::cerr << "Caching five: "  << _hcache( "five" ) << "\n";
+    
+    std::cerr << "Getting one: "   << _hcache( "one" ) << "\n";
+    std::cerr << "Caching six: "   << _hcache( "six" ) << "\n";
+    std::cerr << "Count: " << _count << "\n";
+    for ( auto& _key : _hcache ) { print_key( _key ); }
+    std::cerr << "\n";
+}
+     
+int main()
+{
+    test_tree();
+    test_hash();
+    return 0;
+}
