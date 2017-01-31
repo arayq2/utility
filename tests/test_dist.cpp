a6b7e7c8ac553d6824d78a902e7703f6d8a18367
Arjun Ray <arayq2@gmail.com>
2017-01-30 23:36:50 -0500

    clarify
    differentiate two uses of size_t

--- tests/test_dist.cpp
+++ tests/test_dist.cpp
@@ -2,14 +2,15 @@
 #include "Distributor.h"
 #include <iostream>
 
-using Distributor = Utility::Distributor<size_t>;
+using Index       = size_t; 
+using Distributor = Utility::Distributor<Index>;
 
 void output_lists( std::ostream& os, Distributor const& dist )
 {
-    dist.for_each( [&os]( size_t total, std::vector<size_t> const& list ) -> void
+    dist.for_each( [&os]( size_t total, std::vector<Index> const& list ) -> void
     {
         os << "[" << total << "]";
-        for ( auto& _index : list ) { os << "," << _index; }
+        for ( auto const& _index : list ) { os << "," << _index; }
         os << std::endl;
     } );
 }
@@ -17,7 +18,7 @@ void output_lists( std::ostream& os, Distributor const& dist )
 int main()
 {
     Distributor     _dist(100);
-    size_t          _index(0);
+    Index           _index(0);
     size_t          _size;
     
     while ( std::cin >> _size ) { _dist.assign( _index++, _size ); }
