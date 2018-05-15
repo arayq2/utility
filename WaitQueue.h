e308e7c10271ff20b00c9d9e920d7aa89165b580
Arjun Ray <arayq2@gmail.com>
2018-05-15 16:23:05 -0400

    add to api
    add acquire() and release() entry points.

--- WaitQueue.h
+++ WaitQueue.h
@@ -23,9 +23,11 @@ namespace Utility
         // promises still on the queue will set_exception() on their futures
         ~WaitQueue() noexcept = default;
     
-        void lock() { append( std::this_thread::get_id() ).get(); }
-        
+        void lock()   { append( std::this_thread::get_id() ).get(); }
         void unlock() { remove( std::this_thread::get_id() ); }
+
+        void acquire() { lock(); }
+        void release() { unlock(); }
 
     private:
         using ThdId   = std::thread::id;
