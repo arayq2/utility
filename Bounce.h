eedfc42f6962dbcbb67b3c5e5b22bddbbe1a2b9d
Arjun Ray <arayq2@gmail.com>
2018-08-16 19:24:37 -0400

    improved version
    Generalize the conversion method.

--- Bounce.h
+++ Bounce.h
@@ -9,25 +9,46 @@ namespace Utility
      * Convert callback-style free functions into method calls
      * on an object (class) provided in the argument list. 
      * This saves on boilerplate code and avoids name clutter.
-     */
-    template<typename To, typename From, typename... Args>
+     * For simple pointer recasting cases, the usage will be
+     * like this:
+     * 
+     *     using MyCaster = Caster<MyTo, MyFrom>;
+     *
+     *     template<typename... Args>
+     *     using MyBounce = Bounce<MyCaster, Args...>;
+     *
+     * In more complex conversion cases, the Converter::cast
+     * function will have to be defined explicitly, with 
+     * internal typedefs of to_type and from_type.
+     */     
+    template<typename To, typename From>
+    struct Caster
+    {
+        using to_type   = To;
+        using from_type = From;
+        
+        static to_type* cast( from_type* from )
+        {
+            return reinterpret_cast<to_type*>(from);
+        }
+    };
+    
+    template<typename Converter, typename... Args>
     struct Bounce
     {
-        static To* to_ptr( From* from )
-        {
-            return reinterpret_cast<To*>(from);
-        }
-
+        using To   = typename Converter::to_type;
+        using From = typename Converter::from_type;
+        
         template<typename Retval, Retval (To::* Method)(Args...)>
         static Retval rv_fn( From* from, Args... args )
         {
-            return (to_ptr( from )->*Method)( std::forward<Args>(args)... );
+            return (Converter::cast( from )->*Method)( std::forward<Args>(args)... );
         }
 
         template<void (To::* Method)(Args...)>
         static void vd_fn( From* from, Args... args )
         {
-            (to_ptr( from )->*Method)( std::forward<Args>(args)... );
+            (Converter::cast( from )->*Method)( std::forward<Args>(args)... );
         }
     };
 
