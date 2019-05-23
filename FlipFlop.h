1996b6744e95e2483b01ee1fc1d04441fa6c542a
Arjun Ray <arayq2@gmail.com>
2019-05-23 18:02:10 -0400

    cleanup
    minor change in semantics, bug fix.

--- FlipFlop.h
+++ FlipFlop.h
@@ -5,6 +5,15 @@
 namespace Utility
 {
 
+    /**
+     * FlipFlop.
+     * Race condition arbitration between two cooperating events, 
+     * represented as boolean transitions from false to true.
+     *
+     * Each event is informed of the status of the other. Exactly 
+     * one of the calls to flip() and flop() will return false
+     * (the race winner), and the other, true (the loser).
+     */
     class FlipFlop
     {
     public:
@@ -16,21 +25,22 @@ namespace Utility
         bool flip()
         {
             SpinLock    _lock(flag_);
-            flop_ = true;
-            return flip_;
+            flip_ = true;
+            return flop_;
         }
 
         bool flop()
         {
             SpinLock    _lock(flag_);
-            flip_ = true;
-            return flop_;
+            flop_ = true;
+            return flip_;
         }
 
         bool reset()
         {
             SpinLock    _lock(flag_);
             if ( !flip_ or !flop_ ) { return false; }
+            flip_ = flop_ = false;
             return true;
         }
 
